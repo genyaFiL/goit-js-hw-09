fc493ba71c6a1940b645e1fe5a9072f12b4d1936
genyaFiL <115738087+genyaFiL@users.noreply.github.com>
2023-03-22 18:35:20 +0000

    Deploying to gh-pages from @ genyaFiL/goit-js-hw-09@2d481b7e53991668189e66e369d44fe036404fcc 🚀

---
+++ 01-color-switcher.91882092.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("[data-start]"),r=document.querySelector("[data-stop]");r.setAttribute("disabled",!0),e.addEventListener("click",(()=>{timerId=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),e.setAttribute("disabled",!0),r.removeAttribute("disabled")})),r.addEventListener("click",(()=>{clearInterval(timerId),e.removeAttribute("disabled"),r.setAttribute("disabled",!0)}));
+//# sourceMappingURL=01-color-switcher.91882092.js.map
